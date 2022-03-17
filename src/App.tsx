8a92c481a0d5a90488d7aa3c767bf2b50b729a7d
Yohan M <inuyasha59260@gmail.com>
2022-03-17 14:34:02 +0100

    liste_de_card_sans_flip_sans_CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import FlashcardList from './components/flashcardlist';
+
+const SAMPLE_CARDS = [
+  {
+    id : 1,
+    question: 'What is 3 + 3 ?',
+    answer : '6',
+    options: ['2','3','5','6'],
+
+  },
+  {
+    id : 1,
+    question: 'What is 4 + 4',
+    answer : '8',
+    options: ['2','3','5','8'],
+
+  },
+  {
+    id : 1,
+    question: 'What is 5 + 5',
+    answer : '10',
+    options: ['2','3','5','10'],
+
+  }
+
+]
 
 function App() {
+  const [cards,setCards] = useState(SAMPLE_CARDS)
   return (
     <div className="App">
       <header className="App-header">
@@ -19,6 +46,9 @@ function App() {
           Learn React
         </a>
       </header>
+      <body>
+<h1><FlashcardList flashcards = {cards}/></h1>
+      </body>
     </div>
   );
 }
