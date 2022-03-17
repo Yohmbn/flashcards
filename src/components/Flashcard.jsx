b0ce2254b60058369ed550540aef6b1c31a6dde1
Yohan M <inuyasha59260@gmail.com>
2022-03-17 16:53:54 +0100

    avec_flip_et_reponses_sans_radio_buttons

--- src/components/Flashcard.jsx
+++ src/components/Flashcard.jsx
@@ -1,16 +1,48 @@
 import React, { useState } from 'react'
 
+
  const Flashcard = ({ flashcard }) => {
-     const [flip, setFlip] = useState(false)
+  const [flip, setFlip] = useState(false)
+  const [value, setValue] = React.useState('');
+  const [error, setError] = React.useState(false);
+  const [helperText, setHelperText] = React.useState('Choose wisely');
+
+  const handleRadioChange = (event) => {
+    setValue(event.target.value);
+    setHelperText(' ');
+    setError(false);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    if (value === 'best') {
+      setHelperText('You got it!');
+      setError(false);
+    } else if (value === 'worst') {
+      setHelperText('Sorry, wrong answer!');
+      setError(true);
+    } else {
+      setHelperText('Please select an option.');
+      setError(true);
+    }
+  };
+
+  //<div className="flashcard-option">{option}</div>
 
      return (
          <div className={`card ${flip ? 'flip' : ''}`} onClick={() => setFlip(!flip)}>
              <div className="front">
                  {flashcard.question}
                  <div className="flashcard-options">
+                    <form onSubmit={handleSubmit}>
                      {flashcard.options.map(option => {
                          return <div className="flashcard-option">{option}</div>
                      })}
+                     <button sx={{ mt: 1, mr: 1 }} type="submit" variant="outlined">
+                      Check Answer
+                      </button>
+                     </form>
                  </div>
              </div>
              <div className="back">{flashcard.answer}</div>
